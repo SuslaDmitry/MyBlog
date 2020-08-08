1f6a83b955dede75ec5ade4fa8b65d83a1513500
SuslaDmitry <67871011+SuslaDmitry@users.noreply.github.com>
2020-08-08 17:51:47 +0300

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django_db',
         'USER' : 'user_name',
-        'PASSWORD' : 'password',
+        'PASSWORD' : 'Qwerty*123',
         'HOST' : 'localhost',
         'PORT' : '5432',
     }
