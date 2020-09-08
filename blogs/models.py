5fa787e75391dbfa13eb410d8c8340256f382ebd
dima <dima@example.com>
2020-09-09 00:06:12 +0300

    12

--- blogs/models.py
+++ blogs/models.py
@@ -9,3 +9,6 @@ class BlogPost(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.title + ":\n" + self.text
+
+    class Meta:
+        ordering = ['-date_added']
