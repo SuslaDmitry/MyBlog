009274f745b9b25a755599025f14375c67f7cdd7
dima <dima@example.com>
2020-09-09 18:08:05 +0300

    12

--- blogs/models.py
+++ blogs/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class BlogPost(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
+    image = models.ImageField(null=True, blank=True, upload_to='image/')
     date_added = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     public = models.BooleanField(default=False)
