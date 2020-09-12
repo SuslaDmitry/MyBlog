563e8e49cc2ebee8dd5989230ef1a48f2612db51
dima <dima@example.com>
2020-09-13 02:19:20 +0300

    sds

--- Blog/settings.py
+++ Blog/settings.py
@@ -125,15 +125,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
+
 
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
-    ]
-else:
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Мои настройки
 LOGIN_URL = '/users/login/'
