779a4a6a32ffa18e96cf9362ef6cff6158161dd7
dima <dima@example.com>
2020-09-12 17:04:24 +0300

    Work?

--- blogs/views.py
+++ blogs/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import BlogPost
 from .forms import BlogPostForm
-from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, Http404
+import datetime
+
 
 # Create your views here.
 @login_required
@@ -25,11 +26,15 @@ def owner_posts(request):
     else:
         prev_url = ''
 
-    context = {'owner_page': owner_page, 'next_owner_page_url': next_url, 'prev_owner_page_url': prev_url}
+    context = {'owner_page': owner_page,
+               'owner_posts': owner_posts,
+               'next_owner_page_url': next_url,
+               'prev_owner_page_url': prev_url}
     return render(request, 'blogs/my_posts.html', context)
 
+
 def public_posts(request):
-    """Выводит список публичных тем."""
+    """Выводит список публичных тем на главной странице"""
     public_posts = BlogPost.objects.filter(public=True).order_by('-date_added')
     paginator = Paginator(public_posts, 10)
     page_number = request.GET.get('page', 1)
@@ -44,30 +49,38 @@ def public_posts(request):
     else:
         prev_url = ''
 
-    context = {'public_page': public_page, 'next_public_page_url': next_url, 'prev_public_page_url': prev_url}
+    context = {'public_page': public_page,
+               'public_posts': public_posts,
+               'next_public_page_url': next_url,
+               'prev_public_page_url': prev_url}
     return render(request, 'blogs/index.html', context)
+
 
 @login_required
 def new_post(request):
-    """Добавляет новий пост."""
+    """Добавляет новый пост."""
     if request.method != 'POST':
         # Данные не отправлялись; создается пустая форма.
         form = BlogPostForm()
     else:
         # Отправлены данные POST; обработать данные.
-        form = BlogPostForm(request.POST)
+        form = BlogPostForm(request.POST, request.FILES)
         if form.is_valid():
-            new_topic = form.save(commit=False)
-            new_topic.owner = request.user
-            new_topic.save()
-            return HttpResponseRedirect(reverse('blogs:index'))
+            new_post = form.save(commit=False)
+            new_post.owner = request.user
+            new_post.save()
+            if new_post.public:
+                return HttpResponseRedirect(reverse('blogs:index'))
+            else:
+                return HttpResponseRedirect(reverse('blogs:owner_posts'))
     context = {'form': form}
     return render(request, 'blogs/new_post.html', context)
+
 
 @login_required
 def edit_post(request, post_id):
     """Редактирует существующий пост."""
-    post = BlogPost.objects.get(id=post_id)
+    post = get_object_or_404(BlogPost, id=post_id)
     if post.owner != request.user:
         raise Http404
 
@@ -76,10 +89,48 @@ def edit_post(request, post_id):
         form = BlogPostForm(instance=post)
     else:
         # Отправка данных POST; обработать данные.
-        form = BlogPostForm(instance=post, data=request.POST)
+        form = BlogPostForm(instance=post, data=request.POST, files=request.FILES)
         if form.is_valid():
+            post.date_edit = datetime.datetime.now()
             form.save()
-        return HttpResponseRedirect(reverse('blogs:index'))
+        return HttpResponseRedirect(reverse('blogs:owner_posts'))
 
     context = {'post': post, 'form': form}
     return render(request, 'blogs/edit_post.html', context)
+
+
+def post_details(request, post_id):
+    """Выводит пост целиком"""
+    post = get_object_or_404(BlogPost, id=post_id)
+    if post.owner != request.user and post.public != True:
+        raise Http404
+
+    context = {'post': post}
+    return render(request, 'blogs/post_details.html', context)
+
+
+@login_required
+def delete_post(request, post_id):
+    post = get_object_or_404(BlogPost, id=post_id)
+    if post.owner != request.user:
+        raise Http404
+    post.delete()
+    return HttpResponseRedirect(reverse('blogs:owner_posts'))
+
+
+@login_required
+def privat_public(request, post_id):
+    """Публикует/скрывает пост"""
+    post = get_object_or_404(BlogPost, id=post_id)
+    if post.owner != request.user:
+        raise Http404
+    if post.public:
+        post.public = False
+    else:
+        post.public = True
+        post.date_publication = datetime.datetime.now()
+    post.save()
+    if post.public:
+        return HttpResponseRedirect(reverse('blogs:index'))
+    else:
+        return HttpResponseRedirect(reverse('blogs:owner_posts'))
--- users/views.py
+++ users/views.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.contrib.auth.models import Permission
+
 
 # Create your views here.
 def register(request):
@@ -15,13 +17,19 @@ def register(request):
         form = UserCreationForm(data=request.POST)
         if form.is_valid():
             new_user = form.save()
+            # Присваивание пользователю прав на доступ к админ панеле и редактированию постов
+            new_user.is_staff = True
+            permission = Permission.objects.get(codename='change_blogpost')
+            new_user.user_permissions.add(permission)
+            new_user.save()
             # Выполнение входа и перенаправление на домашнюю страницу.
             authenticated_user = authenticate(username=new_user.username,
-            password=request.POST['password1'])
+                                              password=request.POST['password1'])
             login(request, authenticated_user)
             return HttpResponseRedirect(reverse('blogs:index'))
     context = {'form': form}
     return render(request, 'users/register.html', context)
+
 
 def logout_view(request):
     """Завершает сеанс работы с приложением."""
