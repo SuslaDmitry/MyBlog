34e82ab5bbbfa8ab12e444fda4b489de873cfde1
dima <dima@example.com>
2020-09-13 01:56:07 +0300

    if

--- Blog/settings.py
+++ Blog/settings.py
@@ -127,8 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATICFILES_DIRS = []
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Мои настройки
 LOGIN_URL = '/users/login/'
