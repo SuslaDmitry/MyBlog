35ca95a1b365682bb1f8376d7b670ca4a36dcdcc
dima <dima@example.com>
2020-09-13 02:12:12 +0300

    Asd

--- Blog/settings.py
+++ Blog/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
 
 STATIC_URL = '/static/'
 if DEBUG:
@@ -133,7 +133,7 @@ if DEBUG:
         os.path.join(BASE_DIR, 'static')
     ]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Мои настройки
 LOGIN_URL = '/users/login/'
