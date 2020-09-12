582a5bf24d27677d98ebf2316502e81a9687707f
dima <dima@example.com>
2020-09-13 01:49:37 +0300

    Static Root

--- Blog/settings.py
+++ Blog/settings.py
@@ -126,7 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_DIRS = []
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Мои настройки
 LOGIN_URL = '/users/login/'
