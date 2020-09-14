d2b7cc4f45448460c26e520f72a6185ba758321f
dima <dima@example.com>
2020-09-15 01:25:57 +0300

    owner page v2

--- Blog/urls.py
+++ Blog/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),]
+urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }), ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 app_name = 'Blog'
