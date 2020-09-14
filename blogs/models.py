05bfaea29e88439d96426d7ec9fcc0a37c9b4572
dima <dima@example.com>
2020-09-15 00:45:14 +0300

    with models

--- blogs/models.py
+++ blogs/models.py
@@ -3,26 +3,26 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-# class BlogPost(models.Model):
-#     title = models.CharField(max_length=130)
-#     text = models.TextField()
-#     image = models.ImageField(null=True, blank=True)
-#     date_added = models.DateTimeField(auto_now_add=True)
-#     date_edit = models.DateTimeField(auto_now_add=True)
-#     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-#     public = models.BooleanField(default=False)
-#     date_publication = models.DateTimeField(auto_now_add=True)
-#     tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
-#
-#     def __str__(self):
-#         return self.title + ":\n" + self.text
-#
-#     class Meta:
-#         ordering = ['-date_added']
-#
-#
-# class Tag(models.Model):
-#     title = models.CharField(max_length=50)
-#
-#     def __str__(self):
-#         return '{}'.format(self.title)
+class BlogPost(models.Model):
+    title = models.CharField(max_length=130)
+    text = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_edit = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    public = models.BooleanField(default=False)
+    date_publication = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
+
+    def __str__(self):
+        return self.title + ":\n" + self.text
+
+    class Meta:
+        ordering = ['-date_added']
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return '{}'.format(self.title)
