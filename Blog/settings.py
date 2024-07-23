5095563585db16f3160349c689c0cd4634a0cbe9
Dmitro <yasusianchik@gmail.com>
2024-07-23 18:24:36 +0300

    Re

--- Blog/settings.py
+++ Blog/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DATABASE_URL = "postgresql://postgres:cpfYYRsxhrPStJSmfHxMwYyDBjfdYyUo@viaduct.proxy.rlwy.net:16704/railway"
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
