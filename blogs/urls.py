d4d0fbc3df89241f7a9c8575dbb4b093f1358f0c
Dmitro <yasusianchik@gmail.com>
2024-07-23 13:40:57 +0300

    R6

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,45 +1,46 @@
 from django.conf.urls import url
 from django.urls import path
 from . import views
+from django.urls import re_path as url
 
 urlpatterns = [
     # Домашняя страница. Вывод всех публичных постов.
-    re_path(r'^$', views.public_posts,
+    url(r'^$', views.public_posts,
         name='index'),
 
     # Страница для добавления нового поста
-    re_path(r'^new_post/$', views.new_post,
+    url(r'^new_post/$', views.new_post,
         name='new_post'),
 
     # Страница для редактирования записи
-    re_path(r'^edit_post/(?P<post_id>\d+)/$', views.edit_post,
+    url(r'^edit_post/(?P<post_id>\d+)/$', views.edit_post,
         name='edit_post'),
 
     # Удаление поста
-    re_path(r'^delete_post/(?P<post_id>\d+)/$', views.delete_post,
+    url(r'^delete_post/(?P<post_id>\d+)/$', views.delete_post,
         name='delete_post'),
 
     # Страница с личными публикациями
-    re_path(r'^owner_posts/$', views.owner_posts,
+    url(r'^owner_posts/$', views.owner_posts,
         name='owner_posts'),
 
     # Страница для отельного поста
-    re_path(r'^post_details/(?P<post_id>\d+)/$', views.post_details,
+    url(r'^post_details/(?P<post_id>\d+)/$', views.post_details,
         name='post_details'),
 
     # Публикация/приватизация поста
-    re_path(r'^privat_public/(?P<post_id>\d+)/$', views.privat_public,
+    url(r'^privat_public/(?P<post_id>\d+)/$', views.privat_public,
         name='privat_public'),
 
     # Страница для добавления нового tag
-    re_path(r'^new_tag/$', views.new_tag,
+    url(r'^new_tag/$', views.new_tag,
         name='new_tag'),
 
     # Введет все посты, прикрепленные к этому тегу
-    re_path(r'^tag_detail/(?P<tag_id>\d+)/$', views.tag_detail, name='tag_detail'),
+    url(r'^tag_detail/(?P<tag_id>\d+)/$', views.tag_detail, name='tag_detail'),
 
     # Удаление поста
-    re_path(r'^delete_tag/(?P<tag_id>\d+)/$', views.delete_tag,
+    url(r'^delete_tag/(?P<tag_id>\d+)/$', views.delete_tag,
         name='delete_tag'),
 
 ]
