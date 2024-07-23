97cff2c1d57d75445b8a13db84cf27014c612e52
Dmitro <yasusianchik@gmail.com>
2024-07-23 12:18:56 +0300

    R

--- Blog/settings.py
+++ Blog/settings.py
@@ -107,8 +107,12 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DB',
+        'USER': 'user',
+        'PASSWORD': 'qwerty123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
