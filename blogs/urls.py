cdf8f9a8c09feaf7fabf422f812cadfd549c680a
Dmitro <yasusianchik@gmail.com>
2024-07-24 09:15:29 +0300

    asd

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, re_path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # Домашняя страница. Вывод всех публичных постов.
